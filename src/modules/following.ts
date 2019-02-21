198972878383aee33891dad85c151a7c8cb604cf
Joe MacMahon <joe.macmahon@evergreensmartpower.co.uk>
2019-02-21 17:45:13 +0000

    Convert following class to use dependency injected console.log and inline public method

--- src/modules/following.ts
+++ src/modules/following.ts
@@ -2,20 +2,30 @@ import { JsonExtractor } from '@evergreen-smart-power/validation-tools'
 import { Dict } from '../dict'
 import { Module } from '../module'
 import { TwitterClient } from '../twitterApiClient'
+import { dump } from '../userList/dump'
+
+type Injections = {
+  consoleLog: typeof console.log,
+  userListDump: typeof dump
+}
+
+export const defaults: Injections = {
+  consoleLog: console.log,
+  userListDump: dump
+}
 
 export class Following implements Module {
-  constructor (private client: TwitterClient.GetFriends) { }
-
-  public async getFollowing (user: string): Promise<TwitterClient.User[]> {
-    return this.client.getFriends(user)
-  }
+  constructor (
+    private client: TwitterClient.GetFriends,
+    private injections = defaults
+ ) { }
 
   async run (rawParams: Dict<unknown>) {
     const extractor = new JsonExtractor(rawParams)
     const params = {
       ...extractor.stringValue('user')
     }
-    const intersection = await this.getFollowing(params.user)
-    console.log(JSON.stringify(intersection, null, 2))
+    const following = await this.client.getFriends(params.user)
+    this.injections.consoleLog(JSON.stringify(following, null, 2))
   }
 }
--- test/unit/modules/following.test.ts
+++ test/unit/modules/following.test.ts
@@ -1,8 +1,14 @@
-import { expect } from 'chai'
+import { expect, AssertionError } from 'chai'
 import { random } from 'reproducible-random'
-import { Following } from '../../../src/modules/following'
+import { Following, defaults } from '../../../src/modules/following'
 import { TwitterClient } from '../../../src/twitterApiClient'
 import { randomUser } from '../dataGeneration'
+
+const fakeTwitterApiClient = (usersToReturn: TwitterClient.User[]): TwitterClient.GetFriends => ({
+  getFriends: () => {
+    return Promise.resolve(usersToReturn)
+  }
+})
 
 describe('following', () => {
   it('rejects if the Twitter client rejects', () => {
@@ -12,10 +18,10 @@ describe('following', () => {
     }
     const following = new Following(fakeTwitterApiClient)
 
-    return expect(following.getFollowing(random.string(32))).to.be.rejectedWith(err)
+    return expect(following.run({ user: random.string(32) })).to.be.rejectedWith(err)
   })
 
-  it('wraps .getFriends', async () => {
+  it('calls twitterclient.getFriends with the passed username', async () => {
     const usersToReturn: TwitterClient.User[] = [randomUser()]
     const usernameToCall = random.string(32)
     let usernameCalled: string | undefined
@@ -26,9 +32,28 @@ describe('following', () => {
       }
     }
     const following = new Following(fakeTwitterApiClient)
-    const usersReturned = await following.getFollowing(usernameToCall)
+    await following.run({ user: usernameToCall })
 
     expect(usernameCalled).to.equal(usernameToCall)
-    expect(usersReturned).to.equal(usersToReturn)
+  })
+
+  it('should dump to console if no out file param is passed', async () => {
+    const usersToReturn: TwitterClient.User[] = [randomUser()]
+    const apiClient = fakeTwitterApiClient(usersToReturn)
+
+    const logLines: unknown[][] = []
+    const fakeConsoleLog = (...messages: unknown[]) => {
+      logLines.push(messages)
+    }
+
+    const following = new Following(apiClient, { ...defaults, consoleLog: fakeConsoleLog })
+    await following.run({ user: randomUser().screen_name })
+
+    expect(logLines.length).to.equal(1)
+    const firstLogLine = logLines[0]
+    expect(firstLogLine.length).to.equal(1)
+    const firstLogLineFirstPart = firstLogLine[0]
+    if (typeof firstLogLineFirstPart !== 'string') { throw new AssertionError('Log was not a string') }
+    expect(JSON.parse(firstLogLineFirstPart) as unknown).to.deep.equal(usersToReturn)
   })
 })
